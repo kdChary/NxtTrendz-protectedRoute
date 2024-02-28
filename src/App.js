48bb25c1429426957f790123dc22938d0772ddb4
Siva Dinesh K <ksd.ccbp4@gmail.com>
2024-02-28 03:56:04 +0000

    ProtectedRoute component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
 import Home from './components/Home'
 import Products from './components/Products'
 import Cart from './components/Cart'
@@ -11,10 +12,11 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/products" component={Products} />
-    <Route exact path="/cart" component={Cart} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
     <Route component={NotFound} />
+    <Redirect to="NotFound" />
   </Switch>
 )
 
